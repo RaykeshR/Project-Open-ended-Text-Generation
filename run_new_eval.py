9918c3df5ed73144b8a59e762e3efdb31f3e340d
Raykesh <raykesh.ravirooban@reseau.eseo.>
2026-01-11 07:28:05 +0100

    esplion greedy k=50 book On branch dev Your branch is up to date with 'origin/dev'.
    Changes to be committed:
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.2_gpt2-xl._opt-2.7b_coherence_result.json
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.2_gpt2-xl.jsonl
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.2_gpt2-xl_diversity_mauve_gen_length_result.json
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.2_gpt2-xl_simcse_result.json
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.4_gpt2-xl._opt-2.7b_coherence_result.json
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.4_gpt2-xl.jsonl
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.4_gpt2-xl_diversity_mauve_gen_length_result.json
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.4_gpt2-xl_simcse_result.json
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.6_gpt2-xl._opt-2.7b_coherence_result.json
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.6_gpt2-xl.jsonl
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.6_gpt2-xl_diversity_mauve_gen_length_result.json
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.6_gpt2-xl_simcse_result.json
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.8_gpt2-xl._opt-2.7b_coherence_result.json
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.8_gpt2-xl.jsonl
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.8_gpt2-xl_diversity_mauve_gen_length_result.json
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.8_gpt2-xl_simcse_result.json
    	modified:   plots_epsilon_grid_search/analysis_Coh_Likelihood.png
    	modified:   plots_epsilon_grid_search/analysis_Diversity.png
    	modified:   plots_epsilon_grid_search/analysis_MAUVE.png
    	modified:   plots_epsilon_grid_search/analysis_Perplexity.png
    	modified:   run_epsilon_grid_search.py
    	modified:   run_new_eval.py

--- run_new_eval.py
+++ run_new_eval.py
@@ -8,7 +8,7 @@ def main():
     target_dirs = [
         # 'open_text_gen/wikitext_grid_search',
         # 'open_text_gen/wikitext_epsilon_grid_search',
-        'open_text_gen/ollama_results'
+        'open_text_gen/ollama_results',
         # 'open_text_gen/cc_news_epsilon_grid_search',
         # 'open_text_gen/bookcorpus_epsilon_grid_search'
     ]
