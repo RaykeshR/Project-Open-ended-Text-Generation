c8471f4b31f08af3f9b66d1f874a330ee8ac6be3
Raykesh <raykesh.ravirooban@reseau.eseo.>
2026-01-11 13:46:41 +0100

    k=50 cc_news book ppl On branch dev Your branch is ahead of 'origin/dev' by 1 commit.   (use "git push" to publish your local commits)
    Changes to be committed:
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.2_gpt2-xl_perplexity_result.json
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.4_gpt2-xl_perplexity_result.json
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.6_gpt2-xl_perplexity_result.json
    	new file:   open_text_gen/bookcorpus_epsilon_grid_search/bookcorpus_epsilon_k50_alpha0.8_gpt2-xl_perplexity_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.2_gpt2-xl._opt-2.7b_coherence_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.2_gpt2-xl.jsonl
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.2_gpt2-xl_diversity_mauve_gen_length_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.2_gpt2-xl_perplexity_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.2_gpt2-xl_simcse_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.4_gpt2-xl._opt-2.7b_coherence_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.4_gpt2-xl.jsonl
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.4_gpt2-xl_diversity_mauve_gen_length_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.4_gpt2-xl_perplexity_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.4_gpt2-xl_simcse_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.6_gpt2-xl._opt-2.7b_coherence_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.6_gpt2-xl.jsonl
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.6_gpt2-xl_diversity_mauve_gen_length_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.6_gpt2-xl_perplexity_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.6_gpt2-xl_simcse_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.8_gpt2-xl._opt-2.7b_coherence_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.8_gpt2-xl.jsonl
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.8_gpt2-xl_diversity_mauve_gen_length_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.8_gpt2-xl_perplexity_result.json
    	new file:   open_text_gen/cc_news_epsilon_grid_search/cc_news_epsilon_k50_alpha0.8_gpt2-xl_simcse_result.json
    	modified:   run_epsilon_grid_search.py

--- run_epsilon_grid_search.py
+++ run_epsilon_grid_search.py
@@ -41,7 +41,7 @@ def main():
     ]
 
     # Paramètres globaux
-    dataset_name = 'bookcorpus' # wikitext | cc_news | bookcorpus
+    dataset_name = 'cc_news' # wikitext | cc_news | bookcorpus
     dataset_config = 'plain_text' # wikitext-103-raw-v1 | plain_text | plain_text
     dataset_split = 'train' # test | train | train
     num_prefixes = 50         # Nombre d'exemples à générer (100 est standard utilisé pour 'gpt2...'~124M-~1.5B params sinon 5 )
